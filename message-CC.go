5a54850575c3ad9a1c23bfec4e3a77a84d671140
ShadowNight573 <cloudnic573gmail.com>
2020-03-08 18:06:59 -0500

    0.1v
    Updated message-CC: Finished until furthur notice

--- message-CC.go
+++ message-CC.go
@@ -4,20 +4,26 @@
 
 {{$args := parseArgs 2 ""
 	(carg "int" "channel provided")
-    (carg "int" "message provided")}}
+	(carg "int" "message provided")}}
 
 {{$cp := ($args.Get 0)}}
 {{$mp := ($args.Get 1)}}
 
 {{/* message building */}}
-{{$content := "There is no content on this message"}}
-{{$embed := (cembed "description" "There is no embed on this message")}}
+{{$content := ""}}
+{{$embed := ""}}
 
-{{if (getMessage $cp $mp).Content}}
+{{if and ((getMessage $cp $mp).Content) (index (getMessage $cp $mp).Embeds)}}
     {{$content = (getMessage $cp $mp).Content}}
-{{end}}
-{{if (index (getMessage $cp $mp).Embeds 0)}}
     {{$embed = (index (getMessage $cp $mp).Embeds 0)}}
+    {{sendMessage nil (complexMessage "content" (joinStr "" "<#" $cp "> : " (getMessage $cp $mp).Author.String " (" (getMessage $cp $mp).Author.ID ") : " ((getMessage $cp $mp).Timestamp.Parse.Format "1/02/2006 3:04PM") " UTC" "\n\n" ($content)) "embed" ($embed))}}
+{{else}}
+    {{if (getMessage $cp $mp).Content}}
+        {{$content = (getMessage $cp $mp).Content}}
+        {{sendMessage nil (joinStr "" "<#" $cp "> : " (getMessage $cp $mp).Author.String " (" (getMessage $cp $mp).Author.ID ") : " ((getMessage $cp $mp).Timestamp.Parse.Format "1/02/2006 3:04PM") " UTC" "\n\n" ($content))}}
+    {{end}}
+    {{if (index (getMessage $cp $mp).Embeds)}}
+        {{$embed = (index (getMessage $cp $mp).Embeds 0)}}
+        {{sendMessage nil (complexMessage "content" (joinStr "" "<#" $cp "> : " (getMessage $cp $mp).Author.String " (" (getMessage $cp $mp).Author.ID ") : " ((getMessage $cp $mp).Timestamp.Parse.Format "1/02/2006 3:04PM") " UTC") "embed" ($embed))}}
+    {{end}}
 {{end}}
-
-{{sendMessage nil (complexMessage "content" ($content) "embed" ($embed))}}
